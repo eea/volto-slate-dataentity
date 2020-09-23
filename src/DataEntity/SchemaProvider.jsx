c58aa3d0afb0d48d355fe9cd3899426889ef6403
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-09-23 08:38:46 +0300

    Fix schema provider

--- src/DataEntity/SchemaProvider.jsx
+++ src/DataEntity/SchemaProvider.jsx
@@ -6,21 +6,25 @@ class SchemaProvider extends React.Component {
     if (!this.props.provider_data) return schema;
     const provider_data = this.props.provider_data || {};
 
-    console.log('data', provider_data);
-
-    const select_field = 'Ecosystem_level2';
     const choices = Array.from(
-      new Set(provider_data?.[select_field] || []),
+      new Set(Object.keys(provider_data || {})),
     ).map((n) => [n, n]);
 
-    const newSchema = JSON.parse(JSON.stringify(schema));
-    newSchema.properties.column.choices = choices;
+    return {
+      ...schema,
+      properties: {
+        ...schema.properties,
+        column: {
+          ...schema.properties.column,
+          choices,
+        },
+      },
+    };
   };
 
   render() {
-    console.log('render', this.props);
     const { editSchema, children } = this.props;
-    return children(editSchema);
+    return children(this.getSchema(editSchema));
   }
 }
 
